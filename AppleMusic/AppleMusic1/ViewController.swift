b469c0172b2d75c3b70a6ac16dd7c00a704f31f8
bonjin-all <bonjin.app@gmail.com>
2021-03-08 17:23:06 +0900

    start 코드 추가.

---
+++ AppleMusic/AppleMusic1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleMusic
+//
+//  Created by gigas on 2021/03/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
