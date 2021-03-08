1787542227eb7c143a4fcb796205b9a925dde533
bonjin-all <bonjin.app@gmail.com>
2021-03-08 19:40:27 +0900

    list, header 추가.

--- AppleMusic/AppleMusic/Home/HomeViewController.swift
+++ AppleMusic/AppleMusic/Home/HomeViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 
 class HomeViewController: UIViewController {
     // TODO: 트랙관리 객체 추가
+    let trackManager: TrackManager = TrackManager()
+    
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        collectionView.contentInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+//        collectionView.contentInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
     }
 }
 
@@ -23,8 +25,7 @@ extension HomeViewController: UICollectionViewDataSource {
     // 몇개 표시 할까?
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // TODO: 트랙매니저에서 트랙갯수 가져오기
-        
-        return 10
+        return trackManager.tracks.count
     }
     
     // 셀 어떻게 표시 할까?
@@ -33,6 +34,9 @@ extension HomeViewController: UICollectionViewDataSource {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TrackCollecionViewCell", for: indexPath) as? TrackCollecionViewCell else {
             return UICollectionViewCell()
         }
+        
+        let item = trackManager.track(at: indexPath.row)
+        cell.updateUI(item: item)
         return cell
     }
     
@@ -40,8 +44,23 @@ extension HomeViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         switch kind {
         case UICollectionView.elementKindSectionHeader:
+            
+            guard let item = trackManager.todaysTrack else {
+                return UICollectionReusableView()
+            }
+            
+            guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "TrackCollectionHeaderView", for: indexPath) as? TrackCollectionHeaderView else {
+                return UICollectionReusableView()
+            }
+            
+            header.update(with: item)
+            header.tapHandler = { item -> Void in
+                // Player를 띄운다.
+                print("item: \(item.convertToTrack()?.title)")
+            }
+            
             // TODO: 헤더 구성하기
-            return UICollectionReusableView()
+            return header
         default:
             return UICollectionReusableView()
         }
