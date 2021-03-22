6c363ef2142feff0b78507747eb7d22d4b1cd992
bonjin-all <bonjin.app@gmail.com>
2021-03-22 17:12:53 +0900

    Search API 추가.

--- Netflix/Netflix/Search/SearchViewController.swift
+++ Netflix/Netflix/Search/SearchViewController.swift
@@ -35,8 +35,85 @@ extension SearchViewController: UISearchBarDelegate {
         guard let searchTerm = searchBar.text,
               !searchTerm.isEmpty else { return }
         
-        // 네트워킹을 통한 검색
+        SearchAPI.search(searchTerm) { (movies) in
+            
+            print("---> movies: \(movies.count)")
+        }
+    }
+}
+
+class SearchAPI {
+    static func search(_ term: String, completion: @escaping ([Movie]) -> Void) {
+        let session = URLSession(configuration: .default)
         
-        print("---> 검색어: \(searchTerm)")
+        var urlComponents = URLComponents(string: "https://itunes.apple.com/search?")
+        let mediaQuery = URLQueryItem(name: "media", value: "movie")
+        let entityQuery = URLQueryItem(name: "entity", value: "movie")
+        let termQuery = URLQueryItem(name: "term", value: term)
+        
+        urlComponents?.queryItems?.append(mediaQuery)
+        urlComponents?.queryItems?.append(entityQuery)
+        urlComponents?.queryItems?.append(termQuery)
+        
+        let requestURL = urlComponents?.url
+        
+        let dataTask = session.dataTask(with: requestURL!) { (data, response, error) in
+            let successRange = 200..<300
+            
+            guard error == nil,
+                  let statusCode = (response as? HTTPURLResponse)?.statusCode,
+                  successRange.contains(statusCode) else {
+                completion([])
+                return
+            }
+            
+            guard let resultData = data else {
+                completion([])
+                return
+            }
+            
+            // data
+            let movies = SearchAPI.parseMovies(resultData)
+            completion(movies)
+        }
+        dataTask.resume()
+    }
+    
+    static func parseMovies(_ data: Data) -> [Movie] {
+        let decoder = JSONDecoder()
+        
+        do {
+            let response = try decoder.decode(Response.self, from: data)
+            let movies = response.movies
+            return movies
+            
+        } catch let error {
+            print("---> parsing error: \(error.localizedDescription)")
+            return []
+        }
+    }
+}
+
+struct Response: Codable {
+    let resultCount: Int
+    let movies: [Movie]
+    
+    enum CodingKeys: String, CodingKey {
+        case resultCount
+        case movies = "results"
+    }
+}
+
+struct Movie: Codable {
+    let title: String
+    let director: String
+    let thumbnailPath: String
+    let previeUrl: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case director = "artistName"
+        case thumbnailPath = "artworkUrl100"
+        case previeUrl = "previewUrl"
     }
 }
