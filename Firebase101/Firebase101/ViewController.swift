0a0f9a9f4eb5f0a955ac7fe6ac6846e133638bcc
bonjin-all <bonjin.app@gmail.com>
2021-04-26 14:47:57 +0900

    init firebase

---
+++ Firebase101/Firebase101/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Firebase101
+//
+//  Created by gigas on 2021/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
